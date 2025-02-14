d01853d0c49dc757272fe4b7181796695a44db83
mansiwalia <mansiwalia2192003@gmail.com>
2025-02-14 17:18:18 +0530

    made changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1000 KB (1 MB)
+  },
+});
